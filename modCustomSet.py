8b74d1ac8b9e120ad7d37fff6ff42c95c861edf6
Priyanka Aryal <aryalpr@mnstate.edu>
2016-04-25 09:55:23 -0500

    Added masson to the group

--- modCustomSet.py
+++ modCustomSet.py
@@ -2,9 +2,10 @@ __author__="Priyanka Aryal, Rojina Amatya, Elias Eid"
 __date__="4/22/2016"
 
 """
-Priyanka - 1, 4, 7
-Rojina - 2, 5, 8
-Elias - 3, 6, 9
+Priyanka - 1, 4 
+Rojina - 2, 5
+Elias - 3, 9
+Masson - 6, 7, 8
 """
 class CustomSet:
     def __init__(self, tmpSetList):
