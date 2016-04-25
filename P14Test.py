1334acdf349d695ca92c979087172de343dada09
Priyanka Aryal <aryalpr@mnstate.edu>
2016-04-25 09:39:49 -0500

    Created Constructor

--- P14Test.py
+++ P14Test.py
@@ -6,3 +6,8 @@ Priyanka - 1, 4, 7
 Rojina - 2, 5, 8
 Elias - 3, 6, 9
 """
+from modCustomSet import *
+s1 = CustomSet([1,3,5,1])
+s2 = CustomSet([1,2,3,4])
+s3 = s1 - s2
+print(s3)
--- modCustomSet.py
+++ modCustomSet.py
@@ -6,3 +6,12 @@ Priyanka - 1, 4, 7
 Rojina - 2, 5, 8
 Elias - 3, 6, 9
 """
+class CustomSet:
+    def __init__(self, tmpSetList):
+        self._setList = []
+        for listElement in tmpSetList:
+            if listElement not in self._setList:
+                self._setList.append(listElement)
+
+
+    
