81c90351901432d549443bc7d9ee3cf4ac5a8535
Priyanka Aryal <aryalpr@mnstate.edu>
2016-04-26 16:24:47 -0500

    added a method to find difference between two sets

--- P14Test.py
+++ P14Test.py
@@ -1,14 +1,18 @@
-__author__="Priyanka Aryal, Rojina Amatya, Elias Eid"
+__author__="Priyanka Aryal, Rojina Amatya, Elias Eid, Masson Price"
 __date__="4/22/2016"
 
 """
-Priyanka - 1, 4 
-Rojina - 2, 5
-Elias - 3, 9
-Masson - 6, 7, 8
+Priyanka - 1. Constructor, 4. Difference of sets
+Rojina - 2. Union of sets,  5. Test for membership 
+Elias - 3. Intersection of sets, 9. String method
+Masson - 6. Determine subset, 7. Determine Superset, 8. Determine number of elements
 """
 from modCustomSet import *
 s1 = CustomSet([1,3,5,1])
 s2 = CustomSet([1,2,3,4])
+
+print("Testing the difference of two sets: ")
 s3 = s1 - s2
-print(s3)
+s4 = s2 - s1
+print("s1 - s2 : ", s3)
+print("s2 - s1 : ", s4)
