1a386ce419fa98ec1cb2d6a549709a65724f2a07
Elias Eid <eidel@mnstate.edu>
2016-04-26 16:46:15 -0500

    added str method and intersection method along with tests for both methods

--- modCustomSet.py
+++ modCustomSet.py
@@ -29,4 +29,33 @@ class CustomSet:
                 
         return newList
 
+    def __str__(self):
+        '''
+        Definition:allow sets to be converted to a string with the elements
+        surrounded by { } --created by Elias Eid
+        Precondition: None
+        Posrcondition: None
+        '''
+
+        tmpStr = '{ '
+        for element in self._setList:
+            if self._setList.index(element) != len(self._setList) - 1:
+                tmpStr += str(element) + ', '
+            else:
+                tmpStr += str(element)
+        tmpStr += ' }'
+        return tmpStr
+
+    def __and__(self, otherSet):
+        '''
+        Definition: returns a new set with elements common to the first and
+        second sets. --created by Elias Eid
+        Precondition: otherSet is a CustomSet
+        Postcondition: None
+        '''
+        interList = [ element for element in self._setList if element in otherSet._setList]
+        newSet = CustomSet(interList)
+
+        return newSet
+
     
